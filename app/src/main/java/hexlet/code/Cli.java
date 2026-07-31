8617445085d3a05a6132895568d3878793264420
gedz <gedz@arnica.pro>
2026-07-31 08:46:02 +0500

    chore: add greeting selection to menu

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -2,16 +2,30 @@ package hexlet.code;
 
 import java.util.Scanner;
 
+/* CLI app: System.out is the GUI here, not logging */
+@SuppressWarnings({"java:S106"})
 public class Cli {
+    private Cli() {
+    }
+
     public static void greetUser() {
-        Scanner scanner  = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Welcome to the Brain Games!");
-        System.out.println("May I have your name?");
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
 
-        String name = scanner.nextLine();
+        String choice = scanner.nextLine();
+
+        System.out.println(String.join(" ", "Your choice:", choice));
+
+        if (choice.equals("1")) {
+            System.out.println("Welcome to the Brain Games!");
+            System.out.println("May I have your name?");
+            String name = scanner.nextLine();
+            System.out.println(String.join(" ", "Hello,", name, "!"));
+        }
+
         scanner.close();
-
-        System.out.println("Hello, " + name + "!");
     }
 }
