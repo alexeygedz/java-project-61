11c9670c23122a047ca95a765b97ca10a9ab60d1
alex <alexeygedz@gmail.com>
2026-08-02 13:40:12 +0500

    "feat: add Even game"

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,5 +1,7 @@
 package hexlet.code;
 
+import hexlet.code.game.EvenGame;
+
 import java.util.Scanner;
 
 /* CLI app: System.out is the GUI here, not logging */
@@ -10,21 +12,35 @@ public class Cli {
 
     public static void greetUser() {
         Scanner scanner = new Scanner(System.in);
+        String userName = "";
 
-        System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet");
-        System.out.println("0 - Exit");
+        while (true) {
+            System.out.println("Please enter the game number and press Enter.");
+            System.out.println("1 - Greet");
+            System.out.println("2 - Even");
+            System.out.println("0 - Exit");
 
-        String choice = scanner.nextLine();
+            String choice = scanner.nextLine();
 
-        System.out.println(String.join(" ", "Your choice:", choice));
+            System.out.println(String.join(" ", "Your choice:", choice));
 
-        if (choice.equals("1")) {
-            System.out.println("Welcome to the Brain Games!");
-            System.out.println("May I have your name?");
-            String name = scanner.nextLine();
-            System.out.println(String.join(" ", "Hello,", name, "!"));
+            if (choice.equals("0")) {
+                System.out.println("Bye!");
+                break;
+            }
+
+            if (choice.equals("1")) {
+                System.out.println("Welcome to the Brain Games!");
+                System.out.println("May I have your name?");
+                userName = scanner.nextLine();
+                System.out.println(String.join(" ", "Hello,", userName, "!"));
+            }
+
+            if (choice.equals("2")) {
+                EvenGame.start(scanner, userName);
+            }
         }
+
 
         scanner.close();
     }
---
+++ app/src/main/java/hexlet/code/game/EvenGame.java
@@ -0,0 +1,43 @@
+package hexlet.code.game;
+
+import java.util.Random;
+import java.util.Scanner;
+
+@SuppressWarnings({"java:S106"})
+public class EvenGame {
+
+    private static final Random RANDOM = new Random();
+
+    private EvenGame() {
+    }
+
+    public static void start(Scanner scanner, String name) {
+
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+
+        for (int i = 0; i < 3; i++) {
+            int number = RANDOM.nextInt(500);
+            String answer;
+
+            if (number % 2 == 0) {
+                answer = "yes";
+            } else {
+                answer = "no";
+            }
+
+            System.out.println(String.join(" ", "Question:", String.valueOf(number)));
+            String userAnswer = scanner.nextLine();
+            System.out.println(String.join(" ", "Your answer:", userAnswer));
+
+            if (answer.equals(userAnswer)) {
+                System.out.println("Correct!");
+            } else {
+                System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.");
+                System.out.println(String.join(" ", "Let's try again,", name, "!"));
+                return;
+            }
+        }
+
+        System.out.println(String.join(" ", "Congratulations,", name, "!"));
+    }
+}
